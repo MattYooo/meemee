f45dbe7905ff060713f363c730c47f2f63e791df
Love4653 <league@WTS11.attlocal.net>
2016-07-06 15:00:06 -0700

    d
    d

---
+++ 3dgame/Assets/scripts/LevelSwitch.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelSwitch : MonoBehaviour {
+	public string nextlvl;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			SceneManager.LoadScene (nextlvl);
+		}
+			
+	}
+}
---
+++ 3dgame/Assets/scripts/bulletbobaggins.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletbobaggins : MonoBehaviour {
+	public float speed;
+	void OnTriggerEnter (Collider col){
+		if (col.gameObject.tag=="Player"){
+			GameObject.Destroy (gameObject);
+		}
+	}
+	void FixedUpdate (){
+		gameObject.transform.Rotate(Vector3.forward*speed);
+	}
+}
