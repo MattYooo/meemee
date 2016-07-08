2bc64ede52863fcab7f293d895a5a77932d67278
Love4653 <league@WTS11.attlocal.net>
2016-07-08 15:00:04 -0700

    d
    d

---
+++ 3dgame/Assets/scripts/goal.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+public class goal : MonoBehaviour {
+	public Text scoretext;
+	private int score=0;
+
+	void OnTriggerEnter (Collider collision){
+		if (collision.gameObject.tag.Equals("Sphere")) {
+			score += 1;
+			scoretext.text = "score is" + score; 
+			Debug.Log ("ey boss");
+		}
+	}
+}
---
+++ 3dgame/Assets/scripts/kick.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class kick : MonoBehaviour {
+	public GameObject Player;
+	public float kickpower;
+	public float radius;
+	void Update(){
+		float distance = Vector3.Distance (transform.position, Player.transform.position);
+		if(distance<radius){if(Input.GetMouseButtonDown(0)){
+				GetComponent<Rigidbody> ().AddForce (transform.forward * kickpower);
+			}}
+	}
+
+}
