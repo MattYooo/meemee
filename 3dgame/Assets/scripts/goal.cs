f43a26f5f27e270977dad611399d5b2a6384ef6b
Love4653 <league@WTS11.attlocal.net>
2016-07-09 15:06:22 -0700

    e
    e

--- 3dgame/Assets/scripts/goal.cs
+++ 3dgame/Assets/scripts/goal.cs
@@ -3,14 +3,12 @@ using System.Collections;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 public class goal : MonoBehaviour {
-	public Text scoretext;
 	private int score=0;
-
+	public GameObject scoreman;
 	void OnTriggerEnter (Collider collision){
 		if (collision.gameObject.tag.Equals("Sphere")) {
-			score += 1;
-			scoretext.text = "score is" + score; 
-			Debug.Log ("ey boss");
+			scoreman.GetComponent <scoreman>().score+=1;
+			GameObject.Destroy (this);
 		}
 	}
 }
---
+++ 3dgame/Assets/scripts/scoreman.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class scoreman : MonoBehaviour {
+	public int score;
+	public GameObject text;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.GetComponent <Text> ().text="score equals " + score;
+	
+	}
+}
